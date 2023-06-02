4bddd000a03733d030b763550c830ce3d96b433f
Tasneem safia <76720686+Tasneem-Abu-Safia@users.noreply.github.com>
2023-06-02 19:27:17 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,15 @@
 @endsection
 
 @section('js')
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PGTEC6SEE8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-PGTEC6SEE8');
+</script>
 @endsection
 
 @section('content')
